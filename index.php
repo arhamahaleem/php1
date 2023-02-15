936eee184537d9a854bd40acbd0d863c770c1aeb
arhamahaleem <125270686+arhamahaleem@users.noreply.github.com>
2023-02-15 11:01:55 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:testdbsqlserver2.database.windows.net,1433; Database = floteq_dev", "serveradmin2", "zxcvbnm1!");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql =("SELECT * from PaymentGateWay where PaymentGateWayID=2");
+    $stmt = $conn->query($sql);
+    $row = $stmt->fetch();
+    echo" tssesesese$row[0] $row[1] $row[2] $row[3] $row[4] $row[5] $row[6]";
+    $conn = NULL;
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "serveradmin2", "pwd" => "zxcvbnm1!", "Database" => "floteq_dev", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:testdbsqlserver2.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
