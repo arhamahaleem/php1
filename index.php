b5842cef8423a5d5ff9c249fdd63e85268c36275
arhamahaleem <arhamahaleem@gmail.com>
2023-02-21 11:25:12 +0500

    new

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
 
+
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:testdbsqlserver2.database.windows.net,1433; Database = floteq_dev", "serveradmin2", "zxcvbnm1!");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql =("INSERT INTO Contact");
+    $stmt = $conn->query($sql);
+    $row = $stmt->insert();
+    echo"  $row[0] $row[1] $row[2] $row[3] ";
+    $conn = NULL;
+    
+//     commit github
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "serveradmin2", "pwd" => "zxcvbnm1!", "Database" => "floteq_dev", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:testdbsqlserver2.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -32,29 +57,10 @@
     </div>
   </div>
 </nav>
-<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:testdbsqlserver2.database.windows.net,1433; Database = floteq_dev", "serveradmin2", "zxcvbnm1!");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql =("INSERT INTO Contact");
-    $stmt = $conn->query($sql);
-    $row = $stmt->insert();
-    echo"  $row[0] $row[1] $row[2] $row[3] ";
-    $conn = NULL;
-    
-//     commit github
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "serveradmin2", "pwd" => "zxcvbnm1!", "Database" => "floteq_dev", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:testdbsqlserver2.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-?>
+
+
+
 
 <div class="container mt-3">
     <h2> Contact us</h2>
@@ -85,10 +91,6 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 </div>  
 </body>
 </html>
-
-
-
-
 
 
 
