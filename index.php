9d5f378c3bba9a7296960233c4bbf5fa3f677ae0
arhamahaleem <arhamahaleem@gmail.com>
2023-03-01 12:16:25 +0500

    new

--- index.php
+++ index.php
@@ -12,28 +12,6 @@
 
 <body style= background-color:aliceblue>
 
-<?php
-//submit into database
- 
-try {
-  $conn = new PDO("sqlsrv:server = tcp:testdbsqlserver2.database.windows.net,1433; Database = floteq_dev", "serveradmin2", "zxcvbnm1!");
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //echo "connected <br>";
-
- $email = $_REQUEST['email'];
- $password = $_REQUEST['password'];
- 
-
-$sql = "INSERT INTO Login (Email,Password )VALUES ('$email',$password')";
-$affected_row =$conn->exec($sql);
-echo $affected_row ;
-
- }
-catch (PDOException $e) {
-  print("Error connecting to SQL Server.");
-  die(print_r($e));
- }
-?>
 <div class="img">
     <img src="image/logo.png" alt="logo">
 
