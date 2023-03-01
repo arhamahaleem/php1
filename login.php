c118757ffde088258545baf7e1e14353e990164e
arhamahaleem <arhamahaleem@gmail.com>
2023-03-01 15:56:48 +0500

    new

--- login.php
+++ login.php
@@ -15,49 +15,30 @@
     <img src="image/logo.png" alt="logo">
 
 </div>
+
 <?php
-// PHP Data Objects(PDO) Sample Code:
-Session_start();
+//submit into database
+ 
 try {
-    $conn = new PDO("sqlsrv:server = tcp:testdbsqlserver2.database.windows.net,1433; Database = floteq_dev", "serveradmin2", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $email = $_REQUEST['email'];
-    $password = $_REQUEST['password'];
+  $conn = new PDO("sqlsrv:server = tcp:testdbsqlserver2.database.windows.net,1433; Database = floteq_dev", "serveradmin2", "zxcvbnm1!");
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "connected <br>";
 
-    if(isset($_POST["login"]))
-    {
-        if(empty($_POST["email"]) || empty($_POST["password"]))
-        {
-            $message = '<label>All filed is required</label>';
-        }
-        else{
-            $query = "SELECT * FROM Person WHERE Email =:email AND Password = password";
-            $statement = $conn -> prepare ($query);
-            $statement -> execute(
-                array(
-                    'email' => $_POST["email"],
-                    'password' => $_POST["password"]
-                )
+ 
+ $email = $_REQUEST['email'];
+ $password = $_REQUEST['password'];
 
-                );
-    
-        }
-        $count = $statement->rowCount();
-        if($count > 0)
-        {
-            $_SESSION["email"] = $_POST ["email"];
-         }
-         else{
-            $message ='<label> Wrong email or password</label>';
-         }
-        
-    }
-}
+$sql = "INSERT INTO Person (Email, Password)VALUES ('$email','$password')";
+$affected_row =$conn->exec($sql);
+echo $affected_row ;
+
+ }
 catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+  print("Error connecting to SQL Server.");
+  die(print_r($e));
+ }
 ?>
+
 
 <div class="container">
     <form action="login.php" method="post">
@@ -72,8 +53,6 @@ catch (PDOException $e) {
     <p>Don't have an account?&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="file:///C:/Users/Dell/Desktop/loginscreen/account.html"> <strong>Create an
      account</strong></a></p> 
      <button class="button" type="submit">Login</button>
-
-
 </div>
 </form>
 </body>
